pydantic/monty

Add stable runtime IDs for suspendable calls (#1)
* Add stable runtime IDs for suspendable calls

Expose an additive host-facing runtime ID substrate for
RunProgress and ReplProgress function and OS call payloads.

Plumb runtime IDs through argument conversion so positional and
keyword payload identity remains observable across suspend/resume
and run-progress dump/load boundaries.

Add unit and rstest-bdd behavioural tests for uniqueness,
continuity, and corrupted payload failure paths.

* Refactor runtime ID plumbing and test coverage

Replace tuple-based host call conversion with a named HostCallArgs\nstructure and shared helper functions in args conversion.\n\nAlign keyword key runtime-ID derivation across Inline and Dict kwargs\nby always deriving IDs from Value::id().\n\nAdd and expand runtime-ID tests for:\n- kwargs pairing/order and dump-load stability\n- reused-vs-distinct identity semantics\n- non-call runtime_ids() behavior\n- corruption baseline validation\n\nExtend BDD coverage with keyword-argument and OS-call runtime-ID\nscenarios, and centralize duplicated runtime_ids match logic via a\nshared helper macro used by both RunProgress and ReplProgress.

* Include runtime IDs in consumed function calls

Extend RunProgress::into_function_call and ReplProgress::into_function_call\nto return arg/kwarg runtime ID vectors alongside payload args/kwargs.\n\nThis removes the regression where consuming function-call extraction dropped\nruntime identity metadata, forcing hosts to avoid the primary helper API.\n\nUpdate all in-repo call sites and add a regression test that verifies\ninto_function_call exposes the same runtime IDs as runtime_ids() for the\nsame serialized progress payload.

* Preserve runtime IDs across call snapshots

Carry runtime IDs through run/repl/function-call/OS-call conversion\npaths and include them in Python snapshot payloads so hosts can\ntrack identity continuity across pause/resume and dump/load.\n\nRefactor argument/progress builders an

crates/monty-python/tests/test_os_calls.py
@@ -29,6 +29,25 @@ def test_path_exists_yields_oscall():
     assert result.function_name == snapshot('Path.exists')
     assert result.args == snapshot((PurePosixPath('/tmp/test.txt'),))
     assert result.kwargs == snapshot({})
+    assert len(result.arg_runtime_ids) == len(result.args)
+    assert result.kwarg_runtime_ids == snapshot([])
+
+
+def test_os_call_runtime_ids_survive_snapshot_dump_load():
+    """OS-call runtime IDs are preserved when dumping/loading MontySnapshot."""
+    m = pydantic_monty.Monty('from pathlib import Path; Path("/tmp/test.txt").exists()')
+    result = m.start()
+
+    assert isinstance(result, pydantic_monty.MontySnapshot)
+    expected_arg_runtime_ids = tuple(result.arg_runtime_ids)
+    expected_kwarg_runtime_ids = tuple(result.kwarg_runtime_ids)
+
+    dumped = result.dump()
+    loaded = pydantic_monty.MontySnapshot.load(dumped)
+
+    assert loaded.is_os_function is True
+    assert tuple(loaded.arg_runtime_ids) == expected_arg_runtime_ids
+    assert tuple(loaded.kwarg_runtime_ids) == expected_kwarg_runtime_ids
 
 
 def test_path_stat_yields_oscall():

leynos/full-monty

crates/monty-python/tests/test_os_calls.py
@@ -29,6 +29,24 @@ def test_path_exists_yields_oscall():
     assert result.function_name == snapshot('Path.exists')
     assert result.args == snapshot((PurePosixPath('/tmp/test.txt'),))
     assert result.kwargs == snapshot({})
+    assert len(result.arg_runtime_ids) == len(result.args)
+    assert result.kwarg_runtime_ids == snapshot([])
+
+def test_os_call_runtime_ids_survive_snapshot_dump_load():
+    """OS-call runtime IDs are preserved when dumping/loading FunctionSnapshot."""
+    m = pydantic_monty.Monty('from pathlib import Path; Path("/tmp/test.txt").exists()')
+    result = m.start()
+
+    assert isinstance(result, pydantic_monty.FunctionSnapshot)
+    expected_arg_runtime_ids = tuple(result.arg_runtime_ids)
+    expected_kwarg_runtime_ids = tuple(result.kwarg_runtime_ids)
+
+    dumped = result.dump()
+    loaded = pydantic_monty.FunctionSnapshot.load(dumped)
+
+    assert loaded.is_os_function is True
+    assert tuple(loaded.arg_runtime_ids) == expected_arg_runtime_ids
+    assert tuple(loaded.kwarg_runtime_ids) == expected_kwarg_runtime_ids
 
 
 def test_path_stat_yields_oscall():